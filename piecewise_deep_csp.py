cbe9f19dfc2c92b5fc0859113a4a1c08a10b4cfb
Bogdan <likan26.09@yandex.ru>
2016-01-26 19:00:37 +0300

    Armijo rule#1

--- piecewise_deep_csp.py
+++ piecewise_deep_csp.py
@@ -1,31 +1,13 @@
 import numpy as np
 import scipy as sp
 from scipy.io import loadmat
+from scipy import optimize
 import theano
 import theano.tensor as T
 from scipy import signal
 from sklearn import cross_validation
 from sklearn.linear_model import LogisticRegression as LR
 from theano.compile.debugmode import DebugMode
-
-
-# Load dataset
-data = loadmat('sp1s_aa')
-x = data['x_train']
-y = np.array(data['y_train'], dtype=int)
-y=y.transpose()
-train_indexes,test_indexes = cross_validation.train_test_split(np.arange(y.size), test_size=0.2,random_state=0)
-x_train=x[:,:,train_indexes]
-x_test=x[:,:,test_indexes]
-y_train=y[train_indexes]
-y_test=y[test_indexes]
-
-# Band-pass filter signal
-samp_rate = 100.
-(b, a) = signal.butter(5, np.array([8., 30.]) / (samp_rate / 2.), 'band')
-x_train_filt = signal.filtfilt(b, a, x_train, axis=0)
-x_test_filt  = signal.filtfilt(b, a, x_test, axis=0)
-
 def man_filter(x,y,beta):
     x_t = x[:,:,np.squeeze(y).astype(bool)]
     x_nt = x[:,:,~(np.squeeze(y).astype(bool))]
@@ -91,8 +73,37 @@ def classify_csp(W, V, x_train_filt, y_train, x_test_filt, y_test):
     sc = logistic.score(fte.transpose(), y_test[:,0])
 
     return sc
+def armijo_rule(cost,g):
+    sigma = 0.1
+    beta = 0.5
+    a=0.01
+
+
+
+
+# Load dataset
+data = loadmat('sp1s_aa')
+x = data['x_train']
+y = np.array(data['y_train'], dtype=int)
+y=y.transpose()
+train_indexes,test_indexes = cross_validation.train_test_split(np.arange(y.size), test_size=0.2,random_state=0)
+x_train=x[:,:,train_indexes]
+x_test=x[:,:,test_indexes]
+y_train=y[train_indexes]
+y_test=y[test_indexes]
+
+# Band-pass filter signal
+samp_rate = 100.
+(b, a) = signal.butter(5, np.array([8., 30.]) / (samp_rate / 2.), 'band')
+x_train_filt = signal.filtfilt(b, a, x_train, axis=0)
+x_test_filt  = signal.filtfilt(b, a, x_test, axis=0)
+
+
 
 W = csp(x_train_filt, y_train) #(CH x 5)
+
+# W = man_filter(x_train_filt,y_train,0.5)
+# W = W[:,0:5]
 V = np.ones((x.shape[0],1)) #(T x 1)
 sc = classify_csp(W, V, x_train_filt, y_train, x_test_filt, y_test)
 
@@ -106,7 +117,8 @@ x_test_filt_T  = theano.shared(x_test_filt.transpose(2, 0, 1))
 y_train_T      = T.cast( theano.shared(y_train[:,0]), 'int32')
 y_test_T       = T.cast( theano.shared(y_test[:,0]) , 'int32')
 
-lr         = .01 # learning rate
+# lr         = 0.01 # learning rate
+lr = T.scalar('lr')
 batch_size = y_train.size/4
 epochs     = 2500
 index      = T.lscalar('index')
@@ -121,7 +133,7 @@ variance   = T.tensordot(layer0_out, avg_v, axes=[1,0])
 
 layer1_out = T.log((variance))[:,:,0]
 layer2     = LogisticRegression(input=layer1_out, n_in=5, n_out=2)
-cost       = layer2.negative_log_likelihood(y)+.01*T.sum(T.pow(avg_v,2)) + 1000*(T.sgn(T.min(avg_v)) - 1)*T.pow(T.min(avg_v),2)
+cost       = layer2.negative_log_likelihood(y)+.01*T.sum(T.pow(avg_v,2)) - 1000*(T.sgn(T.min(avg_v)) - 1)*T.pow(T.min(avg_v),2)
 
 params  = [csp_w, avg_v] + layer2.params
 
@@ -131,7 +143,7 @@ for param_i, grad_i in zip(params,grads):
     updates.append((param_i, param_i - lr*grad_i))
 
 
-train_model = theano.function([index], cost, updates=updates,
+train_model = theano.function([index,lr], cost, updates=updates,
       givens={
           X: x_train_filt_T[index * batch_size: (index + 1) * batch_size],
           y: y_train_T[index * batch_size: (index + 1) * batch_size]})
@@ -141,13 +153,14 @@ train_model = theano.function([index], cost, updates=updates,
 test_model = theano.function([], layer2.errors(y), givens = {
         X: x_test_filt_T, y: y_test_T})
 
-num_gradient = theano.function([index], grads[0], mode = 'DebugMode',
+num_gradient = theano.function([index], [grads[0],grads[1],grads[2],grads[3]], mode = 'DebugMode',
                                givens = {X: x_train_filt_T[index * batch_size: (index + 1) * batch_size],
                                          y: y_train_T[index * batch_size: (index + 1) * batch_size]})
 
 for i in range(epochs):
     for j in range(y_train.size/batch_size):
-        cost_ij = train_model(j)
+        # lr = sp.optimize.line_search(train_model(j),)
+        cost_ij = train_model(j,0.01)
         #num_gradij = num_gradient(j)
 
 
